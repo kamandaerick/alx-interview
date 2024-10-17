ee420e569a26a159bed43ca439886c66d2e81df6
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-17 20:58:47 +0300

    Minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,13 +10,11 @@ def minOperations(n):
     '''
     if n <= 1:
         return 0
-
     operations = 0
+    divisor = 2
     while n > 1:
-        if n % 2 == 0:
-            n = n / 2
-            operations += 1
-        else:
-            n -= 1
-            operations += 1
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
     return operations
