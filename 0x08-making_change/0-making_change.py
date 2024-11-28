1fe2d02754608c2efba29effe22f67ef9f8c1050
Kamanda <info.proresearchers@gmail.com>
2024-11-28 06:30:54 +0000

    Version 2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,23 @@
-#!usr/bin/python3
-"""This algorithm find the minimum number of
-coins for a given total"""
+#!/usr/bin/python3
+"""
+Interview Question on: fewest number of coins needed to
+meet a given amount total
+"""
+
 
 def makeChange(coins, total):
+    """ fewest number of coins needed to meet total """
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                dp[i] = min(dp[i], 1 + dp[i - coin])
-    return dp[total] if dp[total] != float('inf') else -1
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
